fb58657f7e278dc02f02184d96e8bab55004e88a
wunlei <flowersonviolent@gmail.com>
2020-10-13 01:46:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let positiveNum = Math.abs(n);
+    let strNum = positiveNum.toString();
+    let result = "";
+    for (let i = 0; i < strNum.length; i++) {
+        result = strNum[i] + result;
+    }
+    return parseInt(result, 10);
+};

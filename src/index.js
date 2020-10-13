b1a91cab3150beded33b9faed7e5d1434b458135
wunlei <confusioncosmos@gmail.com>
2020-10-13 19:55:49 +0300

    refactor: refactor solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 module.exports = function reverse(n) {
-    let positiveNum = Math.abs(n);
-    let strNum = positiveNum.toString();
-    let result = "";
-    for (let i = 0; i < strNum.length; i++) {
-        result = strNum[i] + result;
-    }
-    return parseInt(result, 10);
+    const positiveNum = Math.abs(n);
+    const reversed = positiveNum.toString().split("").reverse().join("");
+    return Number(reversed);
 };
